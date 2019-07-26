4b750968bcbbdb088abb769b4963bc85fedf1c32
noogler617 <themolenator@gmail.com>
2019-07-26 19:14:29 -0400

    added style to the header and landing page

--- react-website-2019/src/components/landingpage.js
+++ react-website-2019/src/components/landingpage.js
@@ -7,8 +7,8 @@ class Landing extends Component {
       <div style={{width: '100%', margin: 'auto'}}>
         <Grid className="landing-grid">
           <Cell col={12}>
-          <img src=""
-           alt="avatar" />
+          <img src="https://external-preview.redd.it/rc-eT1150M_U35EUVHaJxaWXRSze4ekXbfoK1wonKh0.gif?format=mp4&s=26721f3cfbf3909eb1515c5b716a2340281744d9"
+           alt="milky way" />
           </Cell>
         </Grid>
       </div>
