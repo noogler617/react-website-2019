c42577f5ef5c50ea20af9ac1cd2b2aa4272acd6c
noogler617 <themolenator@gmail.com>
2019-07-28 23:29:03 -0400

    edited landing page

--- react-website-2019/src/components/landingpage.js
+++ react-website-2019/src/components/landingpage.js
@@ -16,8 +16,23 @@ class Landing extends Component {
           }}
           src="https://res.cloudinary.com/dlyhbsnfb/video/upload/v1564364034/milky-way/milky_way_dlopql.mp4"
         />
-        <div class="banner-text">
+        <div className="banner-text">
           <h1>Full Stack Web Developer</h1>
+          <p>HTML/CSS | Javascript | React | Python | NodeJS | MongoDB | MySQL</p>
+        </div>
+        <div className="social-links">
+        {/* LinkedIn */}
+        <a href="https://www.linkedin.com/in/ryan-k-mills/" rel="noopener noreferrer" target="_blank">
+        <i className="fa fa-linkedin-square" aria-hidden="true" />
+        </a>
+        {/* Github*/}
+        <a href="https://github.com/noogler617" rel="noopener noreferrer" target="_blank">
+        <i className="fa fa-github-square" aria-hidden="true" />
+        </a>
+        {/* Youtube */}
+        <a href="" rel="noopener noreferrer" target="_blank">
+        <i className="fa fa-youtube-square" aria-hidden="true" />
+        </a>
         </div>
       </div>
     );
