d0ce473e031004aa51b2d24df91cbd4364e82de9
noogler617 <themolenator@gmail.com>
2019-08-02 21:43:44 -0400

    edit project.js file

--- react-website-2019/src/components/projects.js
+++ react-website-2019/src/components/projects.js
@@ -5,7 +5,22 @@ class Projects extends Component {
   constructor(props) {
     super(props);
     this.state = {activeTab: 0};
+
+    if(this.state.activeTab === 1) {
+      return(<div><h1>This is React</h1></div>)
+    }
+    if(this.state.activeTab === 2) {
+      return(<div><h1>This is MongoDB</h1></div>)
+    }
+    if(this.state.activeTab === 3) {
+      return(<div><h1>This is Python</h1></div>)
+    }
+    if(this.state.activeTab === 4) {
+      return(<div><h1>This is Raspberry Pi</h1></div>)
+    }
+
   }
+
   render() {
     return(
       <div className="category-tabs">
