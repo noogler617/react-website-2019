0d59340c64f1d462c581945ca550c827c517cc79
noogler617 <themolenator@gmail.com>
2019-08-12 20:11:41 -0400

    updated npm and removed exitsting modules then reinstalled project dependencies

--- react-website-2019/src/components/contact.js
+++ react-website-2019/src/components/contact.js
@@ -1,62 +1,43 @@
 import React, { Component } from 'react';
-import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+import { Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
 
 
 class Contact extends Component {
   render() {
     return(
-      <div className="contact-body">
-        <Grid className="contact-grid">
-          <Cell col={6}>
-            <h2>Ryan Mills</h2>
-            <img
-              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
-              alt="avatar"
-              style={{height: '250px'}}
-               />
-             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
-
-          </Cell>
-          <Cell col={6}>
-            <h2>Contact Me</h2>
-            <hr/>
-
-            <div className="contact-list">
-              <List>
-                <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-phone-square" aria-hidden="true"/>
-                    (123) 456-7890
-                  </ListItemContent>
-                </ListItem>
-
-                <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-fax" aria-hidden="true"/>
-                    (123) 456-7890
-                  </ListItemContent>
-                </ListItem>
-
-                <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-envelope" aria-hidden="true"/>
-                    someone@example.com
-                  </ListItemContent>
-                </ListItem>
-
-                <ListItem>
-                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
-                    <i className="fa fa-skype" aria-hidden="true"/>
-                    MySkypeID
-                  </ListItemContent>
-                </ListItem>
+      <Container className="contact-grid">
+      <Row>
+        <Col>
+          <h1>Ryan Mills</h1>
+        </Col>
+      </Row>
 
 
-              </List>
-            </div>
-          </Cell>
-        </Grid>
-      </div>
+        <Col xs="6">
+        <Form className="form-grid">
+        <FormGroup>
+        <Label for="exampleEmail">Firstname</Label>
+            <Input type="email" name="email" id="exampleEmail" placeholder="First Name..." />
+        </FormGroup>
+        <FormGroup>
+            <Label for="examplePassword">Lastname</Label>
+            <Input type="password" name="password" id="examplePassword" placeholder="Last Name..." />
+        </FormGroup>
+        <FormGroup>
+        <Label for="exampleEmail">Email</Label>
+            <Input type="email" name="email" id="exampleEmail" placeholder="Email..." />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleText">Text Area</Label>
+          <Input type="textarea" name="text" id="exampleText" />
+        </FormGroup>
+        <Button>Submit</Button>
+        </Form>
+        </Col>
+        </Container>
+
+
+
     )
   }
 }
